5ad6033666f827d43ce2451467c0a6cebbbab5de
Theresa-L <theresa.lachtner@proton.me>
2025-08-15 15:00:27 +0200

    Added headers to files

---
+++ src/main/target/CY8CPROTO/config.c
@@ -0,0 +1,11 @@
+/*
+ * File: config.c
+ * Date: 2025-08-15
+ * Author: Theresa Lachtner
+ *
+ * Project: iNAV for PsoC6
+ * 
+ * Description:
+ *   This file contains configurations specific to the CY8PROTO device used for the project.
+ *
+ */
---
+++ src/main/target/CY8CPROTO/target.c
@@ -0,0 +1,11 @@
+/*
+ * File: target.c
+ * Date: 2025-08-15
+ * Author: Theresa Lachtner
+ *
+ * Project: iNAV for PsoC6
+ * 
+ * Description:
+ *   Target specific definitions.
+ *
+ */
